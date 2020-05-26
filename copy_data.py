efe44dba9eb9dd89328d8e6544700b13e5c99c13
sojip <sojipquantin@gmail.com>
2020-05-26 17:30:39 +0100

    copy all data

--- copy_data.py
+++ copy_data.py
@@ -26,30 +26,67 @@ def copydata(driver, wait, earning, date):
         show_more_button = driver.find_element_by_id("showmore")
         show_more_button.click()
         element = wait.until(EC.presence_of_element_located((By.ID, "morecalendar")))
+        del element
     
     except NoSuchElementException:
         try:
-            #copy data        
+            #copy data
             tickers = driver.find_elements_by_class_name('ticker')
-            for i in range (len(tickers)):
-                time = driver.find_elements_by_class_name("time")
-                tickers_ = driver.find_elements_by_class_name('ticker')
-                if tickers_[i].text != '':
-                    earning['ticker'].append(tickers_[i].text)
-                    earning['date'].append(date)
-                    earning['time'].append(time[i].text)
+            time_title = driver.find_element_by_id("time")
+            actual_title = driver.find_element_by_id("actual")
+            if time_title.is_displayed():
+                for i in range (len(tickers)):       
+                    time = driver.find_elements_by_class_name('time')
+                    tickers_ = driver.find_elements_by_class_name('ticker')
+                    if tickers_[i].text != '':
+                        earning['ticker'].append(tickers_[i].text)
+                        earning['date'].append(date)
+                        earning['time'].append(time[i].text)
+            elif actual_title.is_displayed():
+                for i in range (len(tickers)):
+                    time = driver.find_elements_by_class_name('time')
+                    actual = driver.find_elements_by_css_selector("div[class^='actual']")
+                    tickers_ = driver.find_elements_by_class_name('ticker')
+                    if i < len(actual):
+                        if tickers_[i].text != '':
+                            earning['ticker'].append(tickers_[i].text)
+                            earning['date'].append(date)
+                            earning['time'].append(actual[i].text)
+                    elif i >= len(actual):
+                        if tickers_[i].text != '':
+                            earning['ticker'].append(tickers_[i].text)
+                            earning['date'].append(date)
+                            earning['time'].append(time[i - len(actual)].text)
         except:
             driver.quit()
             return json.dumps("Error copying")
         
     #copy data
     tickers = driver.find_elements_by_class_name('ticker')
-    for i in range (len(tickers)):
-        time = driver.find_elements_by_class_name("time")
-        tickers_ = driver.find_elements_by_class_name('ticker')
-        if tickers_[i].text != '':
-            earning['ticker'].append(tickers_[i].text)
-            earning['date'].append(date)
-            earning['time'].append(time[i].text)
+    time_title = driver.find_element_by_id("time")
+    actual_title = driver.find_element_by_id("actual")
+    if time_title.is_displayed():
+        for i in range (len(tickers)):       
+            time = driver.find_elements_by_class_name('time')
+            tickers_ = driver.find_elements_by_class_name('ticker')
+            if tickers_[i].text != '':
+                earning['ticker'].append(tickers_[i].text)
+                earning['date'].append(date)
+                earning['time'].append(time[i].text)
+    elif actual_title.is_displayed():
+        for i in range (len(tickers)):
+            time = driver.find_elements_by_class_name('time')
+            actual = driver.find_elements_by_css_selector("div[class^='actual']")
+            tickers_ = driver.find_elements_by_class_name('ticker')
+            if i < len(actual):
+                if tickers_[i].text != '':
+                    earning['ticker'].append(tickers_[i].text)
+                    earning['date'].append(date)
+                    earning['time'].append(actual[i].text)
+            elif i >= len(actual):
+                if tickers_[i].text != '':
+                    earning['ticker'].append(tickers_[i].text)
+                    earning['date'].append(date)
+                    earning['time'].append(time[i - len(actual)].text)     
             
     return json.dumps("OK")
