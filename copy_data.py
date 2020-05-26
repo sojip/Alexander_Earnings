b9c6991e15fe0439132d51cc5fec09ab005ecc8b
sojip <sojipquantin@gmail.com>
2020-05-26 02:44:51 +0100

    select all tickers with time adjustment

--- copy_data.py
+++ copy_data.py
@@ -26,81 +26,30 @@ def copydata(driver, wait, earning, date):
         show_more_button = driver.find_element_by_id("showmore")
         show_more_button.click()
         element = wait.until(EC.presence_of_element_located((By.ID, "morecalendar")))
-        #select amc option
-        amc_option = driver.find_element_by_id("amc")
-        driver.execute_script("arguments[0].setAttribute('style','display:inline;')",amc_option)
-        amc_coordinates = amc_option.location
-        driver.execute_script("window.scrollTo(0, arguments[0])", amc_coordinates.get('y'))
-        amc_overlay = driver.find_element_by_class_name("switch-selection")
-        driver.execute_script("arguments[0].style.visibility='hidden'", amc_overlay)
-        amc_option.click()
-
-        #copy data        
-        tickers = driver.find_elements_by_class_name('ticker')
-        for i in range (len(tickers)):
-            tickers_ = driver.find_elements_by_class_name('ticker')
-            if tickers_[i].text != '':
-                earning['ticker'].append(tickers_[i].text)
-                earning['date'].append(date)
-                earning['time'].append('amc')
-
-        #select bmo option
-        bmo_option = wait.until(EC.presence_of_element_located((By.ID, "bmo")))
-        driver.execute_script("arguments[0].setAttribute('style','display:inline;')",bmo_option)
-        bmo_coordinates = bmo_option.location
-        driver.execute_script("window.scrollTo(0, arguments[0])", bmo_coordinates.get('y'))
-        bmo_overlay = driver.find_element_by_class_name("switch-selection")
-        driver.execute_script("arguments[0].style.visibility='hidden'", bmo_overlay)
-        bmo_option.click()
-
-        #copy data
-        tickers = driver.find_elements_by_class_name('ticker')
-        for i in range (len(tickers)):
-            tickers_ = driver.find_elements_by_class_name('ticker')
-            if tickers_[i].text != '':
-                earning['ticker'].append(tickers_[i].text)
-                earning['date'].append(date)
-                earning['time'].append('bmo')
-
-
+    
     except NoSuchElementException:
         try:
-            #select amc option
-            amc_option = driver.find_element_by_id("amc")
-            driver.execute_script("arguments[0].setAttribute('style','display:inline;')",amc_option)
-            amc_coordinates = amc_option.location
-            driver.execute_script("window.scrollTo(0, arguments[0])", amc_coordinates.get('y'))
-            amc_overlay = driver.find_element_by_class_name("switch-selection")
-            driver.execute_script("arguments[0].style.visibility='hidden'", amc_overlay)
-            amc_option.click()
-
             #copy data        
             tickers = driver.find_elements_by_class_name('ticker')
             for i in range (len(tickers)):
+                time = driver.find_elements_by_class_name("time")
                 tickers_ = driver.find_elements_by_class_name('ticker')
                 if tickers_[i].text != '':
                     earning['ticker'].append(tickers_[i].text)
                     earning['date'].append(date)
-                    earning['time'].append('amc')
-
-            #select bmo option
-            bmo_option = wait.until(EC.presence_of_element_located((By.ID, "bmo")))
-            driver.execute_script("arguments[0].setAttribute('style','display:inline;')",bmo_option)
-            bmo_coordinates = bmo_option.location
-            driver.execute_script("window.scrollTo(0, arguments[0])", bmo_coordinates.get('y'))
-            bmo_overlay = driver.find_element_by_class_name("switch-selection")
-            driver.execute_script("arguments[0].style.visibility='hidden'", bmo_overlay)
-            bmo_option.click()
-
-            #copy data
-            tickers = driver.find_elements_by_class_name('ticker')
-            for i in range (len(tickers)):
-                tickers_ = driver.find_elements_by_class_name('ticker')
-                if tickers_[i].text != '':
-                    earning['ticker'].append(tickers_[i].text)
-                    earning['date'].append(date)
-                    earning['time'].append('bmo')
+                    earning['time'].append(time[i])
         except:
             driver.quit()
             return json.dumps("Error copying")
+        
+    #copy data
+    tickers = driver.find_elements_by_class_name('ticker')
+    for i in range (len(tickers)):
+        time = driver.find_elements_by_class_name("time")
+        tickers_ = driver.find_elements_by_class_name('ticker')
+        if tickers_[i].text != '':
+            earning['ticker'].append(tickers_[i].text)
+            earning['date'].append(date)
+            earning['time'].append(time[i])
+            
     return json.dumps("OK")
